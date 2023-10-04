e0e7cda5e848333e47f6f815de8786088ccdcdc4
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-10-04 15:52:41 +0300

    appli côté front

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,60 @@
-import { AppBar, Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles';
+import React, { useEffect, useRef } from 'react';
+import Peer from 'peerjs';
 import './App.css';
-import VideoPlayer from './components/VideoPlayer/VideoPlayer';
-import Options from './components/Options/Options';
-import Notification from './components/Notification/Notification';
 
-// const useStyles = makeStyles((theme) => ({
-//   appBar: {
-//     // [theme.breakpoints.down('xs')]: {
-//     //   width: '90%',
-//     // },
-//   },
+const App = () => {
+  const myVideoRef = useRef();
+  const partnerVideoRef = useRef();
+  const peer = new Peer(undefined, {
+    host: '/',
+    port: '5000',
+    path: '/peerjs',
+  });
 
-//   image: {
-//     marginLeft: '15px',
-//   },
-// }));
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+        myVideoRef.current.srcObject = stream;
 
-function App() {
-  // const classes = useStyles();
+        peer.on('call', (incomingCall) => {
+          incomingCall.answer(stream);
+          incomingCall.on('stream', (partnerStream) => {
+            partnerVideoRef.current.srcObject = partnerStream;
+          });
+        });
+      })
+      .catch((error) => {
+        console.error('Error accessing media devices:', error);
+      });
+  }, []);
+
+  const callPeer = () => {
+    const partnerId = prompt('Enter your friend\'s ID');
+    if (partnerId) {
+      navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+        .then((stream) => {
+          const call = peer.call(partnerId, stream);
+          call.on('stream', (partnerStream) => {
+            partnerVideoRef.current.srcObject = partnerStream;
+          });
+        })
+        .catch((error) => {
+          console.error('Error accessing media devices:', error);
+        });
+    }
+  };
 
   return (
-    <div className='wrapper'>
-      <AppBar className='appBar' position='static' color='inherit'>
-        <Typography variant='h2' align='center'>
-          Video Chat
-        </Typography>
-      </AppBar>
-
-      {/* Video Palyer */}
-      <VideoPlayer />
-
-      {/* Options Component */}
-      <div className='app_options'>
-        <Options>
-          {/* Notification Component */}
-          <Notification />
-        </Options>
+    <div className="App">
+      <div className="video-container">
+        <video ref={myVideoRef} className="my-video" muted autoPlay></video>
+        <video ref={partnerVideoRef} className="partner-video" autoPlay></video>
       </div>
-
+      <button className="call-button" onClick={callPeer}>
+        Call a Friend
+      </button>
     </div>
   );
-}
+};
 
 export default App;
