64740eee4b733c4dc84f9d9a67dca924113fd10b
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-10-04 15:53:03 +0300

    appli côté back

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { ContextProvider } from './SocketContext';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ContextProvider>
       <App />
-    </ContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
